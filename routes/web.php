62ad09be010a86a065a153dcbf8f00e94a5a8eaa
P4anix <pancok_1@yahoo.it>
2023-05-16 19:48:17 +0200

    php artisan make:model -cmsrR

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
-
-
+Route::get('/', function() {
+    return 'home page';
+});
 
 
 
@@ -27,11 +27,11 @@ use Illuminate\Support\Facades\Route;
     return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard'); */
 
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-   /*  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); */
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
