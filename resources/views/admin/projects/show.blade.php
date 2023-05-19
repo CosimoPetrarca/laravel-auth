b3a136489f2bd5fb8d349db543c6f947645ea2f7
P4anix <pancok_1@yahoo.it>
2023-05-19 17:42:33 +0200

    fix add img

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -8,8 +8,10 @@
     <h1 class="fs-4 text-secondary my-4 text-center"> Dettaglio Progetto: {{ $project->title }}</h1>
     <hr>
     <h3><strong>Nome Progetto:</strong> {{ $project->title }}</h3>
+    @if ($project->image)
     <img src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->title }}">
-    <p><strong>Descrizione Progetto:</strong> {{ $project->description }}</p>
+    @endif
+        <p><strong>Descrizione Progetto:</strong> {{ $project->description }}</p>
     <p><strong>Data inizio Progetto:</strong> {{ $project->start_date }}</p>
     <p><strong>Data fine Progetto:</strong> {{ $project->end_date }}</p>
     <p><strong>Slug:</strong> {{ $project->slug }}</p>
